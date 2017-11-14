0243a36b0103679209e47ef1cc0cd78cc24a531c
Faris Widyantho <radenfaris@gmail.com>
2017-11-14 15:09:52 +0700

    minor fix

--- models/library.js
+++ models/library.js
@@ -3,15 +3,17 @@ const ObjectID = require('mongodb').ObjectID
 
 class Library{
   constructor(){
-    this.url = 'mongodb://localhost:27017/library';
+    this.url = 'mongodb://localhost:27017/library'
+    this.collectionName = 'books'
   }
   
   getAll(cb){
+    let col = this.collectionName;
     MongoClient.connect(this.url, function(err, db) {
       if(err){
         cb(err, null)
       } else {
-        db.collection('books').find({}).toArray((err, docs)=>{
+        db.collection(col).find({}).toArray((err, docs)=>{
           db.close();
           cb(null, docs)
         })
@@ -21,6 +23,7 @@ class Library{
   }
   
   insertData(req, cb){
+    let col = this.collectionName;
     MongoClient.connect(this.url, function(err, db) {
       if(err){
         cb(err, null)
@@ -30,13 +33,13 @@ class Library{
             title: req.body.title,
             author: req.body.author,
             catagory: req.body.catagory,
-            stock: req.body.stock 
+            stock: Number(req.body.stock) 
           }
         
         // Insert a single document
-        db.collection('books').insertOne(obj, function(err, result) {
+        db.collection(col).insertOne(obj, function(err, result) {
           db.close();
-          cb(null, result)
+          cb(err, result)
         })
         
       }
@@ -44,6 +47,7 @@ class Library{
   }
 
   editData(req, cb){
+    let col = this.collectionName;
     MongoClient.connect(this.url, function(err, db) {
       if(err){
         cb(err, null)
@@ -53,35 +57,34 @@ class Library{
           title: req.body.title,
           author: req.body.author,
           catagory: req.body.catagory,
-          stock: req.body.stock 
+          stock: Number(req.body.stock) 
         }
-      
-      var myquery = { _id: ObjectID(req.params.id) };
-      
-      db.collection("books").updateOne(myquery, newvalues, function(err, result) {
-        db.close();
-        if(err){
-          cb(err, null)
-        } else {
-          cb(null, result)
-        }
-      });
-    }
+        var myquery = { _id: ObjectID(req.params.id) };
+        db.collection(col).updateOne(myquery, newvalues, function(err, result) {
+          db.close();
+          if(err){
+            cb(err, null)
+          } else {
+            cb(null, result)
+          }
+        });
+      }
       
     })
   }
 
   deleteData(req, cb){
+    let col = this.collectionName;
     MongoClient.connect(this.url, function(err, db) {
       if(err){
         cb(err, null)
+      } else {
+        var myquery = { _id: new ObjectID(req.params.id) };
+        db.collection(col).deleteOne(myquery, function(err, result) {
+          db.close();
+          cb(err, result)
+        })  
       }
-      var myquery = { _id: new ObjectID(req.params.id) };
-      db.collection("books").deleteOne(myquery, function(err, result) {
-        db.close();
-        // res.send(result)
-        cb(null, result)
-      })
       
     })
   }
